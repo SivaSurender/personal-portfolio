ea679751fb6e33bb7813cb4044d5751f44da4b8d
SivaSurender <e.surender14@gmail.com>
2023-12-01 21:38:33 +0530

    added about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,30 @@ import React from "react";
 import { styles } from "../styles";
 import { motion } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
+import { services } from "../constants";
+import { Tilt } from "react-tilt";
+import SectionWrapper from "../hoc/SectionWrapper";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
@@ -38,9 +62,13 @@ const About = () => {
         nesciunt facilis eveniet quia, itaque consequatur nemo iusto magni.
         Fugit, fugiat. Omnis, facere?
       </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10"></div>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => {
+          return <ServiceCard key={service.title} index={index} {...service} />;
+        })}
+      </div>
     </>
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
